6d9a7b2d9eb942598e87ada0b0112a1d0530db22
Helberth Alejandro <helberth12@gmail.com>
2022-10-24 23:12:36 -0500

    se agrego parte de una navbar

---
+++ proyecto/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import '../styles/header.css'
+
+export function Navbar() {
+  return (
+    <nav className="header">
+      <div>
+        <h2>Computers Web Store</h2></div>
+      <ul className="info">
+      <li><div>Page 1</div></li>
+      <li><div>Page 2</div></li>
+      <li><div>Sign Up</div></li>
+      <li><div>Login</div></li>
+    </ul>
+    
+    </nav>
+  );
+}
