250a07ae5512a6622f717f2959fa85aafd7821b9
Helberth Alejandro <helberth12@gmail.com>
2022-11-15 22:43:25 -0500

    agregue mas detalles al productDetails

--- proyecto/src/components/products/ProductDetails.js
+++ proyecto/src/components/products/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import MetaData from "../layout/MetaData";
 import { useParams } from "react-router-dom";
@@ -13,6 +13,7 @@ export const ProductDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const alert = useAlert();
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     dispatch(getProductDetails(id));
@@ -21,6 +22,24 @@ export const ProductDetails = () => {
       dispatch(clearErrors());
     }
   }, [dispatch, alert, error, id]);
+
+  const increaseQty = () => {
+    const contador = document.querySelector('.count')
+
+    if(contador.valueAsNumber >= product.stock) return;
+
+    const qty = contador.valueAsNumber+1;
+    setQuantity(qty)
+  }
+
+  const decreaseQty = () => {
+    const contador = document.querySelector('.count')
+
+    if(contador.valueAsNumber <= 1) return;
+
+    const qty = contador.valueAsNumber-1;
+    setQuantity(qty)
+  }
 
   return (
     <>
@@ -66,13 +85,14 @@ export const ProductDetails = () => {
               <hr />
               <p id="product_price">${product.price} </p>
               <div className="stockCounter d-inline">
-                <span className="btn btn-danger minus">-</span>
+                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
                 <input
                   type="number"
                   className="form-control count d-inline"
+                  value={quantity}
                   readOnly
                 />
-                <span className="btn btn-primary plus">+</span>
+                <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
               </div>
               <button
                 type="button"
@@ -92,50 +112,89 @@ export const ProductDetails = () => {
                   {product.stock > 0 ? "In stock" : "Unavaliable"}
                 </span>
               </p>
-              <hr/>
+              <hr />
               <h4 className="mt-2">Description</h4>
               <p>{product.description}</p>
-              <hr/>
-              <p id="provider">Provider by: <strong>{product.provider}</strong></p>
-              <button id="btn_review" type="button" className="btn btn-primary mt-4" 
-              data-toggle="modal" data-target="#ratingModal">Leave your opinion</button>
-              <div className="alert alert-danger mt-5" type="alert">Login to leave your review</div>
-          {/*Mensaje emergente para dejar opinion y calificacion*/}
-          <div className="row mt-2 mb-5">
+              <hr />
+              <p id="provider">
+                Provider by: <strong>{product.provider}</strong>
+              </p>
+              <button
+                id="btn_review"
+                type="button"
+                className="btn btn-primary mt-4"
+                data-toggle="modal"
+                data-target="#ratingModal"
+              >
+                Leave your opinion
+              </button>
+              <div className="alert alert-danger mt-5" type="alert">
+                Login to leave your review
+              </div>
+              {/*Mensaje emergente para dejar opinion y calificacion*/}
+              <div className="row mt-2 mb-5">
                 <div className="rating w-50">
-                  <div className="modal fade" id="ratingModal" tabIndex="-1" role="dialog"
-                  aria-labelledby='ratingModalLabel' aria-hidden="true">
+                  <div
+                    className="modal fade"
+                    id="ratingModal"
+                    tabIndex="-1"
+                    role="dialog"
+                    aria-labelledby="ratingModalLabel"
+                    aria-hidden="true"
+                  >
                     <div className="modal-dialog" role="document">
                       <div className="modal-content">
                         <div className="modal-header">
-                          <h5 className="modal-title" id="ratingModalLabel">Send Review</h5>
-                          <button type="button" className='close' data-dismiss="modal" aria-label='Close'>
+                          <h5 className="modal-title" id="ratingModalLabel">
+                            Send Review
+                          </h5>
+                          <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                          >
                             <span aria-hidden="true">&times;</span>
                           </button>
                         </div>
                         <div className="modal-body">
                           <ul className="stars">
-                            <li className="star"><i className="fa fa-star"></i></li>
-                            <li className="star"><i className="fa fa-star"></i></li>
-                            <li className="star"><i className="fa fa-star"></i></li>
-                            <li className="star"><i className="fa fa-star"></i></li>
-                            <li className="star"><i className="fa fa-star"></i></li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
+                            <li className="star">
+                              <i className="fa fa-star"></i>
+                            </li>
                           </ul>
 
-                          <textarea name="review" id="review" className="form-control mt3"></textarea>
+                          <textarea
+                            name="review"
+                            id="review"
+                            className="form-control mt-3"
+                          ></textarea>
 
-                          <button className="btn my-3 float-right review-btn px-4 text-white" 
-                          data-dismiss="modal" aria-label="Close">Send</button>
-                        
+                          <button
+                            className="btn my-3 float-right review-btn px-4 text-white"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                          >
+                            Send
+                          </button>
                         </div>
                       </div>
                     </div>
-                    </div>
-                    </div>
-                    </div>
-               
-                
-                      
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </>
